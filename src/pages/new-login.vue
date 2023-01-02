0d55a1d3a349f7242ec1080d5057c71b97149d57
SHUVOBROTO BISWAS <93680258+ShuvobrotoBiswas@users.noreply.github.com>
2023-01-02 19:32:19 +0600

    login card added

--- src/pages/new-login.vue
+++ src/pages/new-login.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="my-body">Login</div>
+  <div>
+    <div class="row flex-center q-pa-md">
+      <q-card class="my-card" flat>
+        <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+        <q-card-section>
+          <div class="text-h6">Our Changing Planet</div>
+          <div class="text-subtitle2">by John Doe</div>
+        </q-card-section>
+        <q-card-section>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        </q-card-section>
+      </q-card>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +20,11 @@ export default {
   // name: 'PageName',
 };
 </script>
+<style scoped>
+.my-card {
+  height: 465px;
+  width: 480px;
+  border-radius: 6px;
+  background-color: #f5f5f5;
+}
+</style>
