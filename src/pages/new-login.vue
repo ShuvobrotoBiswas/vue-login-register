c9e0b0fc388718d0791a355067c7ebf7347585c0
SHUVOBROTO BISWAS <93680258+ShuvobrotoBiswas@users.noreply.github.com>
2023-01-02 19:53:10 +0600

    login form done

--- src/pages/new-login.vue
+++ src/pages/new-login.vue
@@ -8,14 +8,32 @@
         <q-card-section>
           <div>Email</div>
           <div>
-            <q-input rounded outlined v-model="email"></q-input>
+            <q-input outlined v-model="email"></q-input>
           </div>
         </q-card-section>
         <q-card-section>
           <div>Password</div>
           <div>
-            <q-input rounded outlined v-model="password"></q-input>
+            <q-input outlined v-model="password"></q-input>
           </div>
+        </q-card-section>
+        <q-card-section align="center">
+          <q-btn
+            unelevated
+            no-caps
+            class="my-submit-btn text-white"
+            label="Login"
+          ></q-btn>
+        </q-card-section>
+        <q-card-section align="center">
+          <q-btn
+            unelevated
+            no-caps
+            flat
+            class="my-forgot-btn"
+            color="blue-9"
+            label="Forgot your password?"
+          ></q-btn>
         </q-card-section>
       </q-card>
     </div>
@@ -44,5 +62,16 @@ export default {
 .login-txt {
   font-weight: 700;
   font-size: 23px;
+}
+.my-submit-btn {
+  padding: 8px 10px;
+  width: 400px;
+  height: 40px;
+  background: #322df0;
+  box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.12), 0px 0px 12px rgba(0, 0, 0, 0.04);
+  border-radius: 4px;
+}
+.my-forgot-btn {
+  width: 400px;
 }
 </style>
